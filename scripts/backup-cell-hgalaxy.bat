7a611bc6f29930a88622c4d7bbbf4ac31abdfffa
Florian Bauer <florian.bauer@gehtnicht.at>
2015-11-19 09:36:39 +0100

    Add devices

---
+++ scripts/backup-cell-hgalaxy.bat
@@ -0,0 +1,74 @@
+@ECHO OFF
+
+SET DeviceName=hgalaxy
+SET BackupTarget=%USERPROFILE%\Pictures\_Mobil\Galaxy
+SET BackupSource=E:
+SET RobocopyBackupExcludeExtra=/XX
+SET CECHO=POWERSHELL /noprofile /nologo Write-Host -ForegroundColor
+
+TITLE Backstage - %DeviceName%
+
+:WaitForDevicePath
+IF NOT EXIST "%BackupSource%\backstage.inf" (
+  %CECHO% Cyan * Plugin %DeviceName%, so %BackupSource%\backstage.inf is available
+  PAUSE
+  GOTO WaitForDevicePath
+)
+
+:WaitForSpecificDevicePath
+TYPE "%BackupSource%\backstage.inf" | FIND "%DeviceName%" > nul
+IF %ERRORLEVEL% EQU 1 (
+  %CECHO% Cyan * Could not verify device %DeviceName%
+  PAUSE
+  GOTO WaitForSpecificDevicePath
+)
+
+IF NOT EXIST "%BackupTarget%" (
+  %CECHO% Red ERROR: Backup Target does not exist: %BackupTarget%
+  EXIT /B 1
+)
+
+%CECHO% DarkGray Now wait for Backup to complete...
+
+
+
+call:runRobocopy "DCIM"
+
+
+
+ECHO.
+%CECHO% Green Backup completed successfully!
+
+%CECHO% Green Done, thanks!
+PAUSE
+goto:eof
+
+
+
+:runRobocopy
+
+  call:runRobocopyMirror %~1 %~2 %~3 %RobocopyBackupExcludeExtra%
+
+goto:eof
+
+
+
+:runRobocopyMirror
+
+  ECHO.
+  %CECHO% Yellow \"Start Backup of '%~1'...\"
+
+  ROBOCOPY /MIR /B /FFT /R:3 /W:10 /Z /NP /NDL %~4 ^
+    "%BackupSource%\%~1" "%BackupTarget%\%~1" ^
+    /XF desktop.ini SyncToy_*.dat Thumbs.db *.tmp *.temp %~2 ^
+    /XD .svn _svn .dthumb .thumbnails .Shared .trash .sync %~3
+
+  IF %ERRORLEVEL% LSS 8 (
+    %CECHO% Green Done
+  ) ELSE (
+    %CECHO% Red ERROR: Robocopy returned exit code %~1
+    %CECHO% DarkGray For details, see http://ss64.com/nt/robocopy-exit.html
+    EXIT /B 1
+  )
+
+goto:eof
---
+++ scripts/backup-machine-ursusmaritimus.bat
@@ -0,0 +1,67 @@
+@ECHO OFF
+
+SET BackupName=Lae12 Hans Win8 NTFS 232GB
+SET ReflectProfile=%USERPROFILE%\Documents\Reflect\%BackupName%.xml
+SET TcVolumeLetter=S:
+SET TcVolumePath=%TcVolumeLetter%\%BackupName%.tc
+SET BackupTargetLetter=T
+SET BackupTarget=%BackupTargetLetter%:
+SET CECHO=POWERSHELL /noprofile /nologo Write-Host -ForegroundColor
+SET TrueCrypt=%ProgramFiles%\TrueCrypt\TrueCrypt.exe
+SET Reflect=%ProgramFiles%\Macrium\Reflect\Reflect.exe
+SET RemoveDrive=%~dp0..\lib\removedrive.exe
+
+TITLE Backstage - %BackupName%
+
+:WaitForTcVolumePath
+IF NOT EXIST "%TcVolumePath%" (
+  %CECHO% Cyan * Plugin Harddisk, so "%TcVolumePath%" is available
+  PAUSE
+  GOTO WaitForTcVolumePath
+)
+
+IF NOT EXIST "%ReflectProfile%" (
+  %CECHO% Red ERROR: Reflect profile does not exist
+  EXIT /B 1
+)
+
+%CECHO% DarkGray Mounting TC volume...
+"%TrueCrypt%" /volume "%TcVolumePath%" /letter %BackupTargetLetter% /history n /quit
+
+IF %ERRORLEVEL% GTR 0 (
+  %CECHO% Red ERROR: TrueCrypt returned exit code %ERRORLEVEL%
+  EXIT /B 2
+)
+
+%CECHO% DarkGray Cleaning up old backup...
+IF EXIST "%BackupTarget%%BackupName%*.mrimg" ( DEL /F /Q "%BackupTarget%%BackupName%*.mrimg")
+
+%CECHO% DarkGray Now wait for Backup to complete...
+"%Reflect%" -e -w -full "%ReflectProfile%"
+
+IF %ERRORLEVEL% GTR 0 (
+  %CECHO% Red ERROR: Reflect returned exit code %ERRORLEVEL%
+  EXIT /B 3
+)
+
+ECHO.
+%CECHO% Green Backup completed successfully!
+
+%CECHO% DarkGray Unmounting TC volume...
+"%TrueCrypt%" /dismount %BackupTargetLetter% /quit
+
+IF %ERRORLEVEL% GTR 0 (
+  %CECHO% Red ERROR: TrueCrypt returned exit code %ERRORLEVEL%
+  EXIT /B 4
+)
+
+%CECHO% DarkGray Unmounting drive %TcVolumeLetter%...
+%RemoveDrive% %TcVolumeLetter% -l -b
+
+IF %ERRORLEVEL% GTR 0 (
+  %CECHO% Red ERROR: RemoveDrive returned exit code %ERRORLEVEL%
+  EXIT /B 5
+)
+
+%CECHO% Green Done, thanks!
+PAUSE
