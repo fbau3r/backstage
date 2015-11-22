5bc73e237eedacfd3752facfd332fcd270c1bc09
Florian Bauer <florian.bauer@gehtnicht.at>
2015-11-22 14:59:54 +0100

    change flitzerIII backup to MTP sync
    WhatsApp\Database\msgstore*.db.crypt is excluded because it fails with error message "Unexpected size of data stream"

--- scripts/backup-cell-flitzerIII.bat
+++ scripts/backup-cell-flitzerIII.bat
@@ -1,77 +1,41 @@
 @ECHO OFF
 
-SET DeviceName=Flitzer III
-SET BackupTarget=Y:\Mobile\Flo
-SET BackupSource=E:
-SET RobocopyBackupExcludeExtra=/XX
+SET BackupName=%~n0.ffs_batch
+SET BackupPath=%~dpn0.ffs_batch
+SET RevisionsDirectory=Y:\Mobile\Flo\_Revisions
 SET CECHO=POWERSHELL /noprofile /nologo Write-Host -ForegroundColor
+SET FreeFileSync=%ProgramFiles%\FreeFileSync\FreeFileSync.exe
 
-TITLE Backstage - %DeviceName%
+TITLE Backstage - %BackupName%
 
-:WaitForDevicePath
-IF NOT EXIST "%BackupSource%\backstage.inf" (
-  %CECHO% Cyan * Plugin %DeviceName%, so %BackupSource%\backstage.inf is available
-  PAUSE
-  GOTO WaitForDevicePath
-)
-
-:WaitForSpecificDevicePath
-TYPE "%BackupSource%\backstage.inf" | FIND "%DeviceName%" > nul
-IF %ERRORLEVEL% EQU 1 (
-  %CECHO% Cyan * Could not verify device %DeviceName%
-  PAUSE
-  GOTO WaitForSpecificDevicePath
-)
-
-IF NOT EXIST "%BackupTarget%" (
-  %CECHO% Red ERROR: Backup Target does not exist: %BackupTarget%
+IF NOT EXIST "%BackupPath%" (
+  %CECHO% Red * Cannot find %BackupPath%
   EXIT /B 1
+)
+
+TYPE "%BackupPath%" | FIND ">%RevisionsDirectory%</VersioningFolder>" > nul
+IF %ERRORLEVEL% EQU 1 (
+  %CECHO% Red * Cannot find config for Revisions directory
+  EXIT /B 2
 )
 
 %CECHO% DarkGray Now wait for Backup to complete...
 
+"%FreeFileSync%" "%BackupPath%"
 
-
-call:runRobocopyMirror "games\com.mojang" "" "_Archived"
-call:runRobocopy "DCIM"
-call:runRobocopy "Recording"
-call:runRobocopy "WhatsApp" ".nomedia"
-
-
+IF %ERRORLEVEL% GTR 0 (
+  %CECHO% Red ERROR: FreeFileSync returned exit code %ERRORLEVEL%
+  EXIT /B 3
+)
 
 ECHO.
 %CECHO% Green Backup completed successfully!
 
+FOR /F %%A IN ('DIR /B /S "%RevisionsDirectory%" ^| FIND /C /V ""') DO SET RevisionsCount=%%A
+IF %RevisionsCount% GTR 0 (
+  %CECHO% Cyan WARN: There are %RevisionsCount% files to be revised in folder
+  %CECHO% Cyan WARN: %RevisionsDirectory%
+)
+
 %CECHO% Green Done, thanks!
 PAUSE
-goto:eof
-
-
-
-:runRobocopy
-
-  call:runRobocopyMirror %~1 %~2 %~3 %RobocopyBackupExcludeExtra%
-
-goto:eof
-
-
-
-:runRobocopyMirror
-
-  ECHO.
-  %CECHO% Yellow \"Start Backup of '%~1'...\"
-
-  ROBOCOPY /MIR /B /FFT /R:3 /W:10 /Z /NP /NDL %~4 ^
-    "%BackupSource%\%~1" "%BackupTarget%\%~1" ^
-    /XF desktop.ini SyncToy_*.dat Thumbs.db *.tmp *.temp %~2 ^
-    /XD .svn _svn .dthumb .thumbnails .Shared .trash .sync %~3
-
-  IF %ERRORLEVEL% LSS 8 (
-    %CECHO% Green Done
-  ) ELSE (
-    %CECHO% Red ERROR: Robocopy returned exit code %~1
-    %CECHO% DarkGray For details, see http://ss64.com/nt/robocopy-exit.html
-    EXIT /B 1
-  )
-
-goto:eof
