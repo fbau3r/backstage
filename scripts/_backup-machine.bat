29cfeaaa29c08ac846ddc0aa2ca337f0604c3e58
Florian Bauer <florian.bauer@derStandard.at>
2016-06-07 13:49:36 +0200

    Extract common script _backup-machine.bat

---
+++ scripts/_backup-machine.bat
@@ -0,0 +1,77 @@
+@ECHO OFF
+
+IF "%BackupName%"=="" (
+    ECHO Error: Variable "BackupName" missing. Setup variable before calling %~nx0
+    EXIT /B 101
+)
+
+REM SET Default values if not overridden
+IF "%ReflectProfile%"==""     ( SET ReflectProfile=%USERPROFILE%\Documents\Reflect\%BackupName%.xml )
+IF "%TcVolumeLetter%"==""     ( SET TcVolumeLetter=S: )
+IF "%TcVolumePath%"==""       ( SET TcVolumePath=%TcVolumeLetter%\%BackupName%.tc )
+IF "%BackupTargetLetter%"=="" ( SET BackupTargetLetter=T )
+IF "%BackupTarget%"==""       ( SET BackupTarget=%BackupTargetLetter%: )
+IF "%TrueCrypt%"==""          ( SET TrueCrypt=%ProgramFiles%\TrueCrypt\TrueCrypt.exe )
+IF "%Reflect%"==""            ( SET Reflect=%ProgramFiles%\Macrium\Reflect\Reflect.exe )
+IF "%RemoveDrive%"==""        ( SET RemoveDrive=%~dp0..\lib\removedrive.exe )
+IF "%ColorConsole%"==""       ( SET ColorConsole=%~dp0..\lib\colorconsole.exe )
+
+IF NOT EXIST %ColorConsole% (
+    ECHO Error: Cannot find ColorConsole at "%ColorConsole%"
+    EXIT /B 201
+)
+
+TITLE Backstage - %BackupName%
+%ColorConsole% {{Magenta}}Backstage - %BackupName%
+
+:WaitForTcVolumePath
+IF NOT EXIST "%TcVolumePath%" (
+  %ColorConsole% {{Cyan}}* Plugin Harddisk, so "%TcVolumePath%" is available
+  PAUSE
+  GOTO WaitForTcVolumePath
+)
+
+IF NOT EXIST "%ReflectProfile%" (
+  %ColorConsole% {{Red}}ERROR: Reflect profile does not exist
+  EXIT /B 1
+)
+
+%ColorConsole% {{DarkGray}}Mounting TC volume...
+"%TrueCrypt%" /volume "%TcVolumePath%" /letter %BackupTargetLetter% /history n /quit
+
+IF %ERRORLEVEL% GTR 0 (
+  %ColorConsole% {{Red}}ERROR: TrueCrypt returned exit code %ERRORLEVEL%
+  EXIT /B 2
+)
+
+%ColorConsole% {{DarkGray}}Cleaning up old backup...
+IF EXIST "%BackupTarget%%BackupName%*.mrimg" ( DEL /F /Q "%BackupTarget%%BackupName%*.mrimg")
+
+%ColorConsole% {{DarkGray}}Now wait for Backup to complete...
+"%Reflect%" -e -w -full "%ReflectProfile%"
+
+IF %ERRORLEVEL% GTR 0 (
+  %ColorConsole% {{Red}}ERROR: Reflect returned exit code %ERRORLEVEL%
+  EXIT /B 3
+)
+
+ECHO.
+%ColorConsole% {{Green}}Backup completed successfully!
+
+%ColorConsole% {{DarkGray}}Unmounting TC volume...
+"%TrueCrypt%" /dismount %BackupTargetLetter% /quit
+
+IF %ERRORLEVEL% GTR 0 (
+  %ColorConsole% {{Red}}ERROR: TrueCrypt returned exit code %ERRORLEVEL%
+  EXIT /B 4
+)
+
+%ColorConsole% {{DarkGray}}Unmounting drive %TcVolumeLetter%...
+%RemoveDrive% %TcVolumeLetter% -l -b
+
+IF %ERRORLEVEL% GTR 0 (
+  %ColorConsole% {{Red}}ERROR: RemoveDrive returned exit code %ERRORLEVEL%
+  EXIT /B 5
+)
+
+%ColorConsole% {{Green}}Done, thanks!
--- scripts/backup-machine-ursusmaritimus.bat
+++ scripts/backup-machine-ursusmaritimus.bat
@@ -1,67 +1,7 @@
 @ECHO OFF
+SETLOCAL
 
+REM Override variable values here
 SET BackupName=Lae12 Hans Win8 NTFS 232GB
-SET ReflectProfile=%USERPROFILE%\Documents\Reflect\%BackupName%.xml
-SET TcVolumeLetter=S:
-SET TcVolumePath=%TcVolumeLetter%\%BackupName%.tc
-SET BackupTargetLetter=T
-SET BackupTarget=%BackupTargetLetter%:
-SET ColorConsole=%~dp0..\lib\colorconsole.exe
-SET TrueCrypt=%ProgramFiles%\TrueCrypt\TrueCrypt.exe
-SET Reflect=%ProgramFiles%\Macrium\Reflect\Reflect.exe
-SET RemoveDrive=%~dp0..\lib\removedrive.exe
 
-TITLE Backstage - %BackupName%
-
-:WaitForTcVolumePath
-IF NOT EXIST "%TcVolumePath%" (
-  %ColorConsole% {{Cyan}}* Plugin Harddisk, so "%TcVolumePath%" is available
-  PAUSE
-  GOTO WaitForTcVolumePath
-)
-
-IF NOT EXIST "%ReflectProfile%" (
-  %ColorConsole% {{Red}}ERROR: Reflect profile does not exist
-  EXIT /B 1
-)
-
-%ColorConsole% {{DarkGray}}Mounting TC volume...
-"%TrueCrypt%" /volume "%TcVolumePath%" /letter %BackupTargetLetter% /history n /quit
-
-IF %ERRORLEVEL% GTR 0 (
-  %ColorConsole% {{Red}}ERROR: TrueCrypt returned exit code %ERRORLEVEL%
-  EXIT /B 2
-)
-
-%ColorConsole% {{DarkGray}}Cleaning up old backup...
-IF EXIST "%BackupTarget%%BackupName%*.mrimg" ( DEL /F /Q "%BackupTarget%%BackupName%*.mrimg")
-
-%ColorConsole% {{DarkGray}}Now wait for Backup to complete...
-"%Reflect%" -e -w -full "%ReflectProfile%"
-
-IF %ERRORLEVEL% GTR 0 (
-  %ColorConsole% {{Red}}ERROR: Reflect returned exit code %ERRORLEVEL%
-  EXIT /B 3
-)
-
-ECHO.
-%ColorConsole% {{Green}}Backup completed successfully!
-
-%ColorConsole% {{DarkGray}}Unmounting TC volume...
-"%TrueCrypt%" /dismount %BackupTargetLetter% /quit
-
-IF %ERRORLEVEL% GTR 0 (
-  %ColorConsole% {{Red}}ERROR: TrueCrypt returned exit code %ERRORLEVEL%
-  EXIT /B 4
-)
-
-%ColorConsole% {{DarkGray}}Unmounting drive %TcVolumeLetter%...
-%RemoveDrive% %TcVolumeLetter% -l -b
-
-IF %ERRORLEVEL% GTR 0 (
-  %ColorConsole% {{Red}}ERROR: RemoveDrive returned exit code %ERRORLEVEL%
-  EXIT /B 5
-)
-
-%ColorConsole% {{Green}}Done, thanks!
-PAUSE
+call %~dp0_backup-machine.bat
