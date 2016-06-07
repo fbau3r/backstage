36b907088c9d0212e20578d04fd6d9fb79542b36
Florian Bauer <florian.bauer@derStandard.at>
2016-06-07 13:55:38 +0200

    Extract common script _backup-cell.bat

---
+++ scripts/_backup-cell.bat
@@ -0,0 +1,59 @@
+@ECHO OFF
+
+IF "%BackupName%"=="" (
+    ECHO Error: Variable "BackupName" missing. Setup variable before calling %~nx0
+    EXIT /B 101
+)
+
+IF "%BackupPath%"=="" (
+    ECHO Error: Variable "BackupPath" missing. Setup variable before calling %~nx0
+    EXIT /B 102
+)
+
+IF "%RevisionsDirectory%"=="" (
+    ECHO Error: Variable "RevisionsDirectory" missing. Setup variable before calling %~nx0
+    EXIT /B 103
+)
+
+REM SET Default values if not overridden
+IF "%FreeFileSync%"==""       ( SET FreeFileSync=%ProgramFiles%\FreeFileSync\FreeFileSync.exe )
+IF "%ColorConsole%"==""       ( SET ColorConsole=%~dp0..\lib\colorconsole.exe )
+
+IF NOT EXIST %ColorConsole% (
+    ECHO Error: Cannot find ColorConsole at "%ColorConsole%"
+    EXIT /B 201
+)
+
+TITLE Backstage - %BackupName%
+%ColorConsole% {{Magenta}}Backstage - %BackupName%
+
+IF NOT EXIST "%BackupPath%" (
+  %ColorConsole% {{Red}}* Cannot find %BackupPath%
+  EXIT /B 1
+)
+
+TYPE "%BackupPath%" | FIND ">%RevisionsDirectory%</VersioningFolder>" > nul
+IF %ERRORLEVEL% EQU 1 (
+  %ColorConsole% {{Red}}* Cannot find config for Revisions directory
+  EXIT /B 2
+)
+
+%ColorConsole% {{DarkGray}}Now wait for Backup to complete...
+
+"%FreeFileSync%" "%BackupPath%"
+
+IF %ERRORLEVEL% GTR 0 (
+  %ColorConsole% {{Red}}ERROR: FreeFileSync returned exit code %ERRORLEVEL%
+  EXIT /B 3
+)
+
+ECHO.
+%ColorConsole% {{Green}}Backup completed successfully!
+
+FOR /F %%A IN ('DIR /B /S "%RevisionsDirectory%" ^| FIND /C /V ""') DO SET RevisionsCount=%%A
+IF %RevisionsCount% GTR 0 (
+  %ColorConsole% {{Cyan}}WARN: There are %RevisionsCount% files to be revised in folder
+  %ColorConsole% {{Cyan}}WARN: %RevisionsDirectory%
+)
+
+%ColorConsole% {{Green}}Done, thanks!
--- scripts/backup-cell-dani.bat
+++ scripts/backup-cell-dani.bat
@@ -1,41 +1,9 @@
 @ECHO OFF
+SETLOCAL
 
+REM Override variable values here
 SET BackupName=%~n0.ffs_batch
 SET BackupPath=%~dpn0.ffs_batch
 SET RevisionsDirectory=Y:\Mobile\Dani\_Revisions
-SET ColorConsole=%~dp0..\lib\colorconsole.exe
-SET FreeFileSync=%ProgramFiles%\FreeFileSync\FreeFileSync.exe
 
-TITLE Backstage - %BackupName%
-
-IF NOT EXIST "%BackupPath%" (
-  %ColorConsole% {{Red}}* Cannot find %BackupPath%
-  EXIT /B 1
-)
-
-TYPE "%BackupPath%" | FIND ">%RevisionsDirectory%</VersioningFolder>" > nul
-IF %ERRORLEVEL% EQU 1 (
-  %ColorConsole% {{Red}}* Cannot find config for Revisions directory
-  EXIT /B 2
-)
-
-%ColorConsole% {{DarkGray}}Now wait for Backup to complete...
-
-"%FreeFileSync%" "%BackupPath%"
-
-IF %ERRORLEVEL% GTR 0 (
-  %ColorConsole% {{Red}}ERROR: FreeFileSync returned exit code %ERRORLEVEL%
-  EXIT /B 3
-)
-
-ECHO.
-%ColorConsole% {{Green}}Backup completed successfully!
-
-FOR /F %%A IN ('DIR /B /S "%RevisionsDirectory%" ^| FIND /C /V ""') DO SET RevisionsCount=%%A
-IF %RevisionsCount% GTR 0 (
-  %ColorConsole% {{Cyan}}WARN: There are %RevisionsCount% files to be revised in folder
-  %ColorConsole% {{Cyan}}WARN: %RevisionsDirectory%
-)
-
-%ColorConsole% {{Green}}Done, thanks!
-PAUSE
+call %~dp0_backup-cell.bat
