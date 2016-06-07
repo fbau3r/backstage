0e669ac325e737254224cf2891d89e22045569e3
Florian Bauer <florian.bauer@derStandard.at>
2016-06-07 13:13:26 +0200

    Create backup-cell-hlifetab

---
+++ scripts/backup-cell-hlifetab.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+SET BackupName=%~n0.ffs_batch
+SET BackupPath=%~dpn0.ffs_batch
+SET RevisionsDirectory=%USERPROFILE%\Pictures\_Mobil\Lifetab\_Revisions
+SET ColorConsole=%~dp0..\lib\colorconsole.exe
+SET FreeFileSync=%ProgramFiles%\FreeFileSync\FreeFileSync.exe
+
+TITLE Backstage - %BackupName%
+
+IF NOT EXIST "%BackupPath%" (
+  %ColorConsole% {{Red}}* Cannot find %BackupPath%
+  EXIT /B 1
+)
+
+TYPE "%BackupPath%" | FIND ">%RevisionsDirectory%</VersioningFolder>" > nul
+IF %ERRORLEVEL% EQU 1 (
+  %ColorConsole% {{Red}}* Cannot find config for Revisions directory
+  EXIT /B 2
+)
+
+%ColorConsole% {{DarkGray}}Now wait for Backup to complete...
+
+"%FreeFileSync%" "%BackupPath%"
+
+IF %ERRORLEVEL% GTR 0 (
+  %ColorConsole% {{Red}}ERROR: FreeFileSync returned exit code %ERRORLEVEL%
+  EXIT /B 3
+)
+
+ECHO.
+%ColorConsole% {{Green}}Backup completed successfully!
+
+FOR /F %%A IN ('DIR /B /S "%RevisionsDirectory%" ^| FIND /C /V ""') DO SET RevisionsCount=%%A
+IF %RevisionsCount% GTR 0 (
+  %ColorConsole% {{Cyan}}WARN: There are %RevisionsCount% files to be revised in folder
+  %ColorConsole% {{Cyan}}WARN: %RevisionsDirectory%
+)
+
+%ColorConsole% {{Green}}Done, thanks!
+PAUSE
