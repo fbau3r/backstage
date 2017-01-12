3b91d5601c84e372cbba83bec794407853824a28
Florian Bauer <florian.bauer@gehtnicht.at>
2017-01-12 17:24:53 +0100

    Use _one_ TC Container for all Att22 backups

--- scripts/backup-machine-tinkerbell.bat
+++ scripts/backup-machine-tinkerbell.bat
@@ -2,7 +2,9 @@
 SETLOCAL
 
 REM Override variable values here
-SET BackupName=Att22 Daniela Win10 NTFS 1360GB
+SET BackupName=Att22 Daniela Win10
+SET TcVolumeLetter=S:
+SET TcVolumePath=%TcVolumeLetter%\Att22.tc
 SET ReflectProfile=D:\Users\Dani\Documents\Reflect\%BackupName%.xml
 
 call %~dp0_backup-machine.bat
