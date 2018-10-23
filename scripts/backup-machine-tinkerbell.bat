0be154ba39f5577976a8e7047e94bf4295bd11a5
Florian Bauer <florian.bauer@gehtnicht.at>
2018-10-23 22:12:48 +0200

    Fix ReflectProfile for tinkerbell

--- scripts/backup-machine-tinkerbell.bat
+++ scripts/backup-machine-tinkerbell.bat
@@ -6,6 +6,6 @@ SET BackupName=Att22 Daniela Win10
 SET VcVolumeLetter=S:
 SET VcVolumePath=%VcVolumeLetter%\Att22.vc
 SET VcKeyPath=%USERPROFILE%\.vc\22tta.key
-SET ReflectProfile=D:\Users\Dani\Documents\Reflect\%BackupName%.xml
+SET ReflectProfile=%USERPROFILE%\Documents\Reflect\%BackupName%.xml
 
 call %~dp0_backup-machine.bat
