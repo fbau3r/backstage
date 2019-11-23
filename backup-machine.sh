467e15538a89ee1e0730d9174bc8799a8d7e5199
Florian Bauer <florian.bauer@gehtnicht.at>
2019-11-23 14:52:25 +0100

    Implement start_backup

--- backup-machine.sh
+++ backup-machine.sh
@@ -3,6 +3,7 @@
 script_dir="$(realpath $(dirname $0))"
 
 backup_drive_name="Backup Container"
+reflect_path="${PROGRAMFILES}/Macrium/Reflect/Reflect.exe"
 removedrive_path="${script_dir}/lib/removedrive.exe"
 veracrypt_drive_letter=T
 veracrypt_path="${PROGRAMFILES}/VeraCrypt/veracrypt.exe"
@@ -18,6 +19,7 @@ source "${backstage_conf_path}"
 
 # assert values and given files exist
 test    "${backup_name:?}"
+test -f "${reflect_profile_path:?}" || { echo -e "\e[31mError\e[0m: Cannot find \"$(realpath "${reflect_profile_path}")\""; exit 11; }
 test -f "${veracrypt_key_file:?}"   || { echo -e "\e[31mError\e[0m: Cannot find \"$(realpath "${veracrypt_key_file}")\"";   exit 12; }
 
 function waitfor_backup_drive() {
@@ -65,6 +67,26 @@ function unmount_backup_volume() {
         || { echo -e "\e[31mError\e[0m: Could not unmount encrypted volume"; exit 202; }
 }
 
+function start_backup() {
+    echo "Starting backup..."
+    "${reflect_path}" -e -w -full "${reflect_profile_path}" \
+    || {
+        echo -e "\e[31mError\e[0m: Could not start backup"
+        echo -e "       Please start \e[36mMacrium Reflect\e[0m and check for errors in \e[36mLog\e[0m tab."
+        return 1
+    }
+}
+
+function start_failure_cleanup() {
+    echo; echo -e "\e[33mHint\e[0m: Starting \e[35mFailure Cleanup\e[0m"
+}
+
+function done_with_errors() {
+    error_code=${1:-1}
+    echo -e "\e[31mDone\e[0m (with Errors above)"
+    exit $error_code
+}
+
 function unmount_backup_drive() {
     echo "Unmounting drive \"${backup_drive}\"..."
     "${removedrive_path}" "${backup_drive}" -l -b \
@@ -73,10 +95,7 @@ function unmount_backup_drive() {
 
 waitfor_backup_drive
 mount_backup_volume
-
-echo "Start backup..."
-echo "start macrium backup"
-
+start_backup || { start_failure_cleanup; unmount_backup_volume; done_with_errors 41; }
 unmount_backup_volume
 unmount_backup_drive
 echo -e "\e[32mDone\e[0m"
