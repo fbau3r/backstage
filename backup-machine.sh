f083f25979c19715ad43018dca7abad116173e45
Florian Bauer <florian.bauer@gehtnicht.at>
2019-11-24 21:27:29 +0100

    Create backup-machine.sh with waitfor_backup_drive

---
+++ backup-machine.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+backup_drive_name="Backup Container"
+veracrypt_drive_letter=T
+veracrypt_path="${PROGRAMFILES}/VeraCrypt/veracrypt.exe"
+
+backstage_conf_path="$(realpath "${USERPROFILE}/.backstage/backstage.conf")"
+if [[ ! -f "${backstage_conf_path}" ]]; then
+    echo -e "\e[31mError\e[0m: Cannot find \"${backstage_conf_path}\""
+    exit 10
+fi
+
+# load machine.conf from user profile
+source "${backstage_conf_path}"
+
+# assert values and given files exist
+test    "${backup_name:?}"
+
+function waitfor_backup_drive() {
+    # wait for mounted backup drive
+    while : ; do
+
+        backup_drive=$( powershell -command "Get-WmiObject -Class Win32_Volume -Filter \"Label like '${backup_drive_name}%'\" | %{ \$_.DriveLetter }" )
+
+        if [[ -n "$backup_drive" ]]; then
+            # https://unix.stackexchange.com/a/26592/278383
+            # Clear everything on the line, regardless of cursor position
+            echo -e "\033[2K\r\e[32mInfo\e[0m: Found backup drive \"${backup_drive_name}\" with drive letter \"${backup_drive}\""
+            break
+        else
+            echo -en "\r\e[36mWaiting\e[0m: Please insert backup drive \"${backup_drive_name}\"... (CTRL+C to abort)"
+            sleep 5s
+        fi
+
+    done
+
+    backup_file="${backup_drive}/${backup_name}"
+    if [[ ! -f "${backup_file}" ]]; then
+        echo -e "\e[31mError\e[0m: Cannot find \"${backup_file}\" on backup drive"
+        exit 101
+    fi
+}
+
+waitfor_backup_drive
+
+echo "Mount backup volume..."
+echo "mount backup volume"
+
+echo "Start backup..."
+echo "start macrium backup"
+
+echo "Unmount encrypted volume..."
+echo "unmount"
+
+echo "Unmount backup drive..."
+echo "unmount"
+echo -e "\e[32mDone\e[0m"
